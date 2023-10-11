9f0be960ecad0305fe2b622d76ab6cef17df03a6
peliades <jvpv@cesar.school>
2023-10-11 16:47:41 -0300

    Feito parte do ponto position

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -2,44 +2,40 @@ import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 
 public class Hero {
-    int position_X;
-    int position_Y;
+
+    private Position position = new Position ();
 
     public Hero (int x, int y){
-        this.position_X = x;
-        this.position_Y = y;
+        this.position.setX(x);
+        this.position.setY(y);
     }
 
     // Getter
     public int[] getCoords() {
-        int[] values = {position_X, position_Y};
+        int[] values = {this.position.getX(), this.position.getY()};
         return values;
     }
 
     // Setter
     public void setCoords (int x, int y) {
-        this.position_X = x;
-        this.position_Y = y;
+        this.position.setX(x);
+        this.position.setY(y);
     }
 
-    public void moveUp () {
-        this.position_Y = this.position_Y - 1;
-    }
+    public void moveUp () { this.position.setY(this.position.getY() - 1);}
 
     public void moveDown () {
-        this.position_Y = this.position_Y + 1;
+        this.position.setY(this.position.getY() + 1);
     }
 
-    public void moveLeft () {
-        this.position_X = this.position_X - 1;
-    }
+    public void moveLeft () { this.position.setX(this.position.getX() - 1);}
 
     public void moveRight () {
-        this.position_X = this.position_X + 1;
+        this.position.setX(this.position.getX() + 1);
     }
 
     public void draw(Screen screen) {
-        screen.setCharacter(this.position_X, this.position_Y, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(this.position.getX(), this.position.getY(), TextCharacter.fromCharacter('X')[0]);
     }
 
 }
